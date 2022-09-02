6ffdeeb2bde3ac17e5c18cb10fb1428a18a218e8
nasirullahnu <nasirullah878@gmail.com>
2022-09-03 03:22:19 +0600

    news catagories added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('connected')
+const newsCatagories = () => {
+    url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCatagories(data.data.news_category))
+}
+
+const displayCatagories = (catagories) => {
+    // console.log(catagories)
+    const catagoriesContainer = document.getElementById('news-catagoried');
+    catagories.forEach(catagori => {
+        console.log(catagori)
+        const catagoriDiv = document.createElement('div')
+        catagoriDiv.classList.add('catagories')
+        catagoriDiv.innerHTML = `
+            <p class='catagories'>${catagori.category_name}</p>
+        `
+        catagoriesContainer.appendChild(catagoriDiv);
+        
+    })
+}
+const hello = () => {
+    console.log('clicked')
+}
+
+newsCatagories();
